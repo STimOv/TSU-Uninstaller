2403b4765239bf21db95071fb0a6515de4ac526d
STimOv <tsvamp333@googlemail.com>
2016-11-23 08:47:29 +0100

    Added
    Initial Add

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SW_Registry
+{
+    partial class Installed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.SnapToGrid;
+            this.listView1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 862);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(843, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Deinstallieren";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Installed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1008, 862);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Installed";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Installed_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace SW_Registry
+{
+    public partial class Installed : Form
+    {
+        public Installed()
+        {
+            InitializeComponent();
+        }
+
+        private void Installed_Load(object sender, EventArgs e)
+        {
+            RegistryKey rk=Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            RegistryKey rktemp;
+            string[] SW_Name=rk.GetSubKeyNames();
+            string[] SW_Publisher = rk.GetSubKeyNames();
+            string[] SW_Version = rk.GetSubKeyNames();
+            int i=0;
+            string uninstall;
+            string[] row = {"","",""};
+            string[] rkstrings = rk.GetSubKeyNames();
+            listView1.View = View.Details;
+            
+            listView1.Columns.Add("Software","Software");
+            listView1.Columns["Software"].Width = 350;
+            foreach (string x in rkstrings)
+            {
+                rktemp = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + x,false);
+                //listBox1.Items.Add("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\" + x);
+                //listView1.Items.Add("");
+
+                SW_Name[i] = (string)rktemp.GetValue("DisplayName", "");
+                
+
+
+                    
+                    rktemp = null;
+                    i++;
+                
+                rktemp = null;
+            }
+
+
+            listView1.Columns.Add("Version", "Version");
+            listView1.Columns["Version"].Width = 150;
+            i = 0;
+
+
+            foreach (string x in rkstrings)
+            {
+                rktemp = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + x, false);
+                //listBox1.Items.Add("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\" + x);
+                //listView1.Items.Add("");
+
+                SW_Version[i] = (string)rktemp.GetValue("DisplayVersion", "");
+
+                
+
+
+                    rktemp = null;
+                    i++;
+                
+                rktemp = null;
+            }
+
+
+            listView1.Columns.Add("Hersteller", "Hersteller");
+            listView1.Columns["Hersteller"].Width = 350;
+            i = 0;
+
+
+            foreach (string x in rkstrings)
+            {
+                rktemp = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + x, false);
+                //listBox1.Items.Add("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\" + x);
+                //listView1.Items.Add("");
+
+                SW_Publisher[i] = (string)rktemp.GetValue("Publisher", "");
+
+ 
+                    
+                    rktemp = null;
+                    i++;
+                
+                rktemp = null;
+            }
+
+            for (i = 0; i < SW_Name.Count(); i++)
+            {
+                row[0] = SW_Name[i];
+                row[1] = SW_Version[i];
+                row[2] = SW_Publisher[i];
+                if (!((row[0].Equals(String.Empty))&&(row[1].Equals(String.Empty))&&(row[2].Equals(String.Empty))))
+                {
+                var listViewItem = new ListViewItem(row);
+                listView1.Items.Add(listViewItem);
+                }
+            }
+            //Resize and Position of Elements
+            listView1.Width = Installed.ActiveForm.Width - 200;
+            button1.Location = new System.Drawing.Point(Installed.ActiveForm.Width - 155, 32);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0,j=0;
+            RegistryKey rk = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            RegistryKey rktemp;
+            string uninstall;
+            string[] rkstrings = rk.GetSubKeyNames();
+            string[] SW_Name = rk.GetSubKeyNames();
+
+            foreach (string x in rkstrings)
+            {
+                rktemp = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + x, false);
+                //listBox1.Items.Add("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\" + x);
+                //listView1.Items.Add("");
+
+                SW_Name[i] = (string)rktemp.GetValue("DisplayName", "");
+
+
+
+
+                rktemp = null;
+                i++;
+
+                rktemp = null;
+            }
+
+            for (j = 0; (j < i); j++)
+            {
+                //MessageBox.Show(listView1.SelectedItems[0].ToString());
+                if (listView1.SelectedItems[0].ToString().Equals("ListViewItem: {" + SW_Name[j] +"}"))
+                {
+                    MessageBox.Show("WARNING: You are uninstalling \""+SW_Name[j]+"\" permanently from your PC!");
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    
+                    
+                    rktemp = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\"+rkstrings[j]+"\\", false);
+                    uninstall = (string)rktemp.GetValue("UninstallString", "nicht gefunden");
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    //uninstall.Replace("MsiExec.exe", "");
+                    //MessageBox.Show(uninstall);
+                    startInfo.Arguments = "/C \""+uninstall+"\"";
+                    //MessageBox.Show(uninstall);
+                    try
+                    {
+                        // Start the process with the info we specified.
+                        // Call WaitForExit and then the using statement will close.
+                        using (Process exeProcess = Process.Start(startInfo))
+                        {
+                            exeProcess.WaitForExit();
+                        }
+                    }
+                    catch
+                    {
+                        // Log error.
+                    }
+                }
+            }
+
+        }
+    }
+}
